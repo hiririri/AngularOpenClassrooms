6a8abed1a22f75bcefbf4816054e8d2047aaacc8
leonoreSarfati <leonore.sarfati@realconcultingdata.com>
2024-04-04 14:33:01 +0200

    ✨ boucle for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnap } from './models/face-snap.model';
+import {NgFor} from '@angular/common';
 
 @Component({
   standalone: true,
@@ -10,15 +11,32 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
+    this.faceSnaps = [
+      {
+      title: 'Archibald',
+      description: 'Mon meilleur ami depuis tout petit !',
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 0
+    },
+      {
+      title: 'Three Rock Mountain',
+      description: 'Un endroit magnifique pour les randonnées.',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: "la montagne",
+    },
+      {
+      title: 'Un bon repas',
+      description: 'Mmmh que c\'est bon !',
+      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      createdDate: new Date(),
+      snaps: 0
+    }
+    ];
   }
 }
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,15 +1,8 @@
 export class FaceSnap {
-  constructor(
-    public title: string,
-    public description: string,
-    public imageUrl: string,
-    public createdDate: Date,
-    public snaps: number
-  ) {
-    this.title = title;
-    this.description = description;
-    this.imageUrl = imageUrl;
-    this.createdDate = createdDate;
-    this.snaps = snaps;
-  }
+  title!: string;
+  description!: string;
+  imageUrl!: string;
+  createdDate!: Date;
+  snaps!: number;
+  location?: string;
 }
